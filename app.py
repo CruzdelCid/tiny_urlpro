46905f64b90dcb7efdb6d554ab06620af88272d4
CruzdelCid <cldelcid@ufm.edu>
2020-11-17 22:15:40 -0600

    Pruebas

--- app.py
+++ app.py
@@ -2,23 +2,18 @@ from flask import Flask, request, render_template
 from redis import Redis 
 
 
+#Diccionario
 dicci = {"1":"uno", "dos":"2"}
-
 print(dicci['1'])
 print(dicci['dos'])
-
-#https://www.w3schools.com/python/python_try_except.asp usemos try y except para evitarnos problemas
 try: 
     print(dicci['4'])
 except:
     print("error")
 
+#https://www.w3schools.com/python/python_try_except.asp usemos try y except para evitarnos problemas
 
 #https://riptutorial.com/es/flask/example/7981/rutas-basicas con esto ponemos convertidores del id en la URL
-
-
-
-
 
 
 """
@@ -29,10 +24,6 @@ user = {"Nombre":"Cruz del Cid", "Carnet":"20200394", "edad":"17", "País":"Guat
 conn.hmset("my_information", user)
 conn.set("alv", "Hola mundo")
 
-
-
-
-
 print(conn.hgetall("my_information"))
 print(type(conn.hgetall("my_information")))
 
@@ -40,20 +31,52 @@ print(conn.hget("my_information", "Nombre"))
 print(conn.get("alv"))
 
 """
+
 app = Flask(__name__)
+#Tiny_URL 
 @app.route("/")
 @app.route("/<string_v>")
 def prueba(string_v=None):
-    if (string_v==None): 
+    if (string_v == None): 
         return "HELLO WORLD"
     else: 
-        return "Palabra: " + string_v
+        return render_template("index.html", string_v = string_v)
+
+
+#URL list
+@app.route("/urls")
+def urls():
+    pass
+
+@app.route("/stats")
+def stats():
+    pass
+
+#administración del equipo
+@app.route("/admin")
+def admin():
+    pass
+
+@app.route("/load")
+def load():
+    pass
+
+#Buscar
+@app.route("/search")
+def search():
+    pass
+
+
+
+
+
 """
     try:
         return "Palabra: " + string_v
     except: 
         return "HELLO WORLD!"
 """
+
 @app.route("/nombre")
 def nombre():
     return "c"
@@ -69,6 +92,7 @@ def edad():
 @app.route("/pais")
 def pais():
     return "Guatemala"
+
 
 if __name__ == "__main__": 
     app.run(host = "0.0.0.0", port = 5000, debug = True)
