4345a2e46ac7e464dc2d3c7520d84e67f1d144c6
CruzdelCid <cldelcid@ufm.edu>
2020-11-19 15:24:39 -0600

    Dos redis

--- prueba.py
+++ prueba.py
@@ -37,42 +37,36 @@ print("")
 print("")
 print("PRUEBA GETALL DE REDIS")
 redis  = Redis('localhost', port=6379, charset="utf-8", decode_responses=True)
-redis_visitas = Redis('localhost', port=7000, charset="utf-8", decode_responses=True)
+#redis_visitas = Redis('localhost', port=7000, charset="utf-8", decode_responses=True)
 
-
-#edis_visitas = Redis('localhost', port=6379, charset="utf-8", decode_responses=True)
+#        print(redis.setnx(f"{key_2}_date", datetime.now().strftime('%d-%m-%Y %H:%M:%S')))
+#redis_visitas = Redis('localhost', port=6379, charset="utf-8", decode_responses=True)
 #redis_date = Redis('localhost', port=6379, charset="utf-8", decode_responses=True)
 
 def comprobar(key_1):
-    if (key_1 in paginas):
-        return True
-    elif(redis.get(key_1) == ""): 
-        return False
-    else: 
-        return True
+    return False
 
 def crear(key_2, valor):
     print("")
     if (comprobar(key_2) == False):
         dicci = {} 
         print(redis.setnx(key_2, valor)) 
-        print(redis.setnx(f"{key_2}_visitas", 0)) 
-        print(redis.setnx(f"{key_2}_date", datetime.now().strftime('%d-%m-%Y %H:%M:%S')))
+        #print(redis_visitas.setnx(key_2, 0))
+
         #f"{date.today()}
         dicci[key_2] = redis.get(key_2)
-        dicci[f"{key_2}_visitas"] = redis.get(f"{key_2}_visitas")
-        dicci[f"{key_2}_date"] = redis.get(f"{key_2}_date")
+        #dicci[f"{key_2}_visitas"] = redis_visitas.get(key_2)
         print(dicci)
     else:
         print("El key ya existe")
     
 
-key = "qwerty0"
+key = "qwerty"
 val = "www.google.com"
 
 
 crear(key, val)
-
+"""
 crear("url", val)
 
 crear("stats", val)
@@ -86,14 +80,19 @@ crear(key, val)
 redis.delete('*')
 redis.delete(f"{key}_visitas")
 redis.delete(f"{key}_date")
-
+"""
 
 print("ALV")
 lista = redis.keys('*')
+#lista_visitas = redis_visitas.key('*')
 print(lista)
+#print(lista_visitas)
 
 for m in lista: 
     redis.delete(m)
+
+#for m in lista_visitas: 
+#    redis_visitas.delete(m)
 
 print(redis.keys('*'))
 print(redis.keys('*'))
